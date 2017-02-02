5bac572daa65900f94b126406ffe9d77408c8bcf
shawn sandy <shawnsandy04@gmail.com>
2017-02-02 14:08:41 -0500

    updates the layout gist page

--- src/resources/views/component/content.blade.php
+++ src/resources/views/component/content.blade.php
@@ -1,14 +1,34 @@
+@php
+use Carbon\Carbon ;
+@endphp
 <div class="gist-collection">
 
-    <h1>{{ $data['description'] }}</h1>
-
+    <h3 class="text-capitalize">
+        <img alt="@{{ $data['owner']['login']  }}" height="30" src="{{ $data['owner']['avatar_url'] }}&amp;s=30" width="30">
+        <a href="/">
+            {{ (!empty($data['description'] )) ? $data['description'] : $data['id'] }}
+        </a>
+    </h3>
+    <hr>
     @foreach($data['files'] as $key => $value)
-        <h3>Filename - {{ $key }}</h3>
-        <hr>
-        <pre style="max-height: 400px; overflow: auto"><code>{{ $value['content'] }}</code></pre>
+        <h4>
+            <a href="{{ $value['raw_url'] }}" target="_blank">{{ $key }}</a> <span class="badge">{{ $value['size'] / 1000 }} KB</span>
+        </h4>
+        <pre style="" class="prettyprint linenums lang-php">
+            {{ $value['content'] }}
+        </pre>
     @endforeach
+
+    Posted {{ Carbon::parse($data['created_at'])->diffForHumans() }} by  {{ ucwords($data['owner']['login'] )}} Updated {{ Carbon::parse($data['updated_at'])->diffForHumans() }}
     <hr>
-    Author:
-    <hr>
+    @push('styles')
+
+    @endpush
+    @push('scripts')
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=sons-of-obsidian"></script>
+    @endpush
+    @push('inline_scripts')
+    @endpush
+
 </div>
 
--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -14,20 +14,5 @@
         </div>
 
     </div>
-    @push('styles')
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
-    @endpush
-    @push('scripts')
 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script>
-    @endpush
-    @push('inline_scripts')
-    <script>
-        $(document).ready(function() {
-            $('pre code').each(function(i, block) {
-                hljs.highlightBlock(block);
-            });
-        });
-    </script>
-    @endpush
 @endsection
