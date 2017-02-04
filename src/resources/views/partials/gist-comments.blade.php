83a32c5d9f069e0caaca0b48fc5e9887ce7de3cc
shawnsandy04 <shawnsandy04@gmail.com>
2017-02-04 07:16:04 -0500

    updated the commment layout

--- src/resources/views/partials/gist-comments.blade.php
+++ src/resources/views/partials/gist-comments.blade.php
@@ -1,12 +1,12 @@
 <h4 class="lead">
-    <img alt="@{{ $comment['userLogin']  }}" height="30"
-         src="{{ $comment['userAvatar'] }}&amp;s=30" width="30">
-
-    {{ $comment['userLogin'] }} <i class="fa fa-clock-o"> {{ $comment['created'] }}</i>
-
-
+<img alt="@{{ $comment['userLogin']  }}" height="30" src="{{ $comment['userAvatar'] }}&amp;s=30" width="30">
+    {{ $comment['userLogin'] }}
 </h4>
 <hr>
 <p>
     {{ $comment['body'] }}
+</p>
+<hr>
+<p>
+    <i class="fa fa-clock-o"></i>{{ $comment['created'] }}
 </p>
