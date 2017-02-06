21d1512d98c47414c6c762cca1d58f5ba774e9e0
shawn sandy <shawnsandy04@gmail.com>
2017-02-06 16:01:39 -0500

    wip -- multiple editon on paeg

--- src/resources/views/component/content.blade.php
+++ src/resources/views/component/content.blade.php
@@ -40,38 +40,5 @@
     </div>
 
 
-    @push('inline-styles')
-    <style type="text/css" media="screen">
-        #editor {
-            /*position: absolute;*/
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-        }
-    </style>
-    @endpush
-    @push('scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.6/ace.js"></script>
-    @endpush
-    @push('inline_scripts')
-    <script>
-
-        var config = document.getElementById('git-edit');
-        var theme = config.dataset.theme;
-        var mode = config.dataset.lang;
-
-        var editor = ace.edit("code");
-        editor.setTheme("ace/theme/" + theme);
-        editor.getSession().setMode("ace/mode/" + mode);
-        editor.getSession().setUseWrapMode(true);
-        editor.setAutoScrollEditorIntoView(true);
-        editor.setOption("maxLines", 30);
-        editor.setOption("minLines", 10);
-        editor.setReadOnly(true);
-
-    </script>
-    @endpush
-
 </div>
 
--- src/resources/views/partials/gist-files.blade.php
+++ src/resources/views/partials/gist-files.blade.php
@@ -3,9 +3,50 @@
     <span class="badge">{{ round($item['size'] / 1000 , 1 )}} KB</span>
 </h4>
 
-<div id="git-edit" data-theme="dawn" data-lang="{{ strtolower($item['language']) }}" class="gist-editor">
-    <div id="code" style="" class="lang-{{ strtolower($item['language']) }}">
+<div id="git-edit" class="gist-editor">
+    <div id="code" style="" class="code" data-theme="dawn" data-lang="{{ strtolower($item['language']) }}" >
         {{ $item['content'] }}
     </div>
 </div>
 
+
+@push('inline-styles')
+<style type="text/css" media="screen">
+    #editor {
+        /*position: absolute;*/
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+</style>
+@endpush
+@push('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.6/ace.js"></script>
+@endpush
+@push('inline_scripts')
+<script>
+  var editor;
+  var theme;
+  var mode;
+
+  $('.code').each(function(){
+        //var config = document.getElementById('git-edit');
+        theme = $(this).data('theme');
+        mode = $(this).data('mode');
+
+        console.log(mode + theme);
+
+        editor = ace.edit(this);
+        editor.setTheme("ace/theme/" + theme);
+        editor.getSession().setMode("ace/mode/" + mode);
+        editor.getSession().setUseWrapMode(true);
+        editor.setAutoScrollEditorIntoView(true);
+        editor.setOption("maxLines", 30);
+        editor.setOption("minLines", 10);
+        editor.setReadOnly(true);
+    })
+
+
+</script>
+@endpush
