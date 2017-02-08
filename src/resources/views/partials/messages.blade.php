eef5fc8fae194ff23070b757df0acb2939283475
shawn sandy <shawnsandy04@gmail.com>
2017-02-08 15:01:05 -0500

    Adds validation error messsage

--- src/resources/views/partials/messages.blade.php
+++ src/resources/views/partials/messages.blade.php
@@ -1,5 +1,11 @@
-@if(Session::has('error'))
-    <p class="alert alert-error">{{ Session::get('error') }}</p>
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
 
 @if(Session::has('success'))
