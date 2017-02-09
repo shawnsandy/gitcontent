0056d8dd634456d0f0ecdcf12daa33c4a301c393
shawnsandy04 <shawnsandy04@gmail.com>
2017-02-08 20:57:36 -0500

    Added validataion display errors

--- src/resources/views/partials/errors.blade.php
+++ src/resources/views/partials/errors.blade.php
@@ -1,3 +1,12 @@
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 @if(Session::has('error'))
     <p class="alert alert-error">{{ Session::get('error') }}</p>
