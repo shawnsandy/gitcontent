a5f790201c6b8b4696fab666f5fa71c5dc18a975
shawnsandy04 <shawnsandy04@gmail.com>
2017-02-09 19:03:09 -0500

    Reanged code

--- src/resources/views/component/ace-editor.blade.php
+++ src/resources/views/component/ace-editor.blade.php
@@ -9,9 +9,6 @@
                 </p>
             </div>
         </div>
-        <div class="code-editor" data-theme="dawn" data-filename="{{$data['filename'] or ''}}" data-readonly="false"
-             data-mode="{{ strtolower(isset($data['language'] ) ? $data["language"] : 'md') }}"> {{ old('content', (isset($data['content']) ? $data['content'] : '// code')) }}</div>
-        <textarea style="display: none" class="form-control" name="files[{{ $data['filename'] or '' }}][content]"
-                  id="content-{{ $data['filename'] or '' }}" cols="30" rows="10"></textarea>
+        @include('gitcontent::component.ace')
     </div>
 </div>
