9e07054b394ba3538479caa2940c67c24712a812
shawnsandy04 <shawnsandy04@gmail.com>
2017-02-11 11:20:22 -0500

    Adds required selector to input fields

--- src/resources/views/partials/add-file-gist.blade.php
+++ src/resources/views/partials/add-file-gist.blade.php
@@ -1,6 +1,6 @@
 <p class="input-group">
     <span class="input-group-addon" id=""><i class="fa fa-file-text"></i> </span>
-    <input type="text" class="form-control input-lg" name="files"
+    <input type="text" class="form-control input-lg required" name="files"
            placeholder="Enter the filename (readme.md) and click [ Attach new file ]">
     <span class="input-group-addon text-uppercase" id="">
                       <button type="submit" class="btn btn-default btn-sm btn-link ">
